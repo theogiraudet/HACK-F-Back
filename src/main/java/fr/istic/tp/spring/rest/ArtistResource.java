91614aa246956a96230fc32dd80351940ac5098d
Théo <theo.giraudet@orange.fr>
2021-12-09 15:28:28 +0100

    Resolve CORS

--- src/main/java/fr/istic/tp/spring/rest/ArtistResource.java
+++ src/main/java/fr/istic/tp/spring/rest/ArtistResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.logging.Logger;
 
 @RestController
 @Controller
+@CrossOrigin
 public class ArtistResource {
 
     private final Logger logger = Logger.getLogger("Artist Ressource REST");
